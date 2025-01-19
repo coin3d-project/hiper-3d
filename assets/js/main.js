bce2d28bb1992ff66e4e4d0a55a465033c2ddc35
pastoikos <paulstoikos@gmail.com>
2025-01-20 01:36:27 +0200

    Fixed icons for drop-down and arrow-up

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,8 +116,8 @@
      */
     on('click', '.mobile-nav-toggle', function(e) {
         select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
+        // this.classList.toggle('bi-list')
+        // this.classList.toggle('bi-x')
     })
 
     /**
@@ -141,8 +141,8 @@
             if (navbar.classList.contains('navbar-mobile')) {
                 navbar.classList.remove('navbar-mobile')
                 let navbarToggle = select('.mobile-nav-toggle')
-                navbarToggle.classList.toggle('bi-list')
-                navbarToggle.classList.toggle('bi-x')
+                // navbarToggle.classList.toggle('bi-list')
+                // navbarToggle.classList.toggle('bi-x')                
             }
             scrollto(this.hash)
         }
